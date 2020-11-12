4907fe502e4dc63c2f30e390f0fba8e07ba97a48
Cruji42 <cruji42@gmail.com>
2020-11-11 22:13:43 -0600

    First Deploy

--- src/enviroment/database.ts
+++ src/enviroment/database.ts
@@ -1,9 +1,9 @@
 import {Pool} from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: '',
-    database: 'LLB',
+    user:'ikdpcqjiaqzovf',
+    host:'ec2-23-20-168-40.compute-1.amazonaws.com',
+    password:'d689f2296a72864be45270a9b0f757de26f77e668474d7b49d9de4d9fca4c07e',
+    database:'d8rdvtof32nulh',
     port: 5432
 })
