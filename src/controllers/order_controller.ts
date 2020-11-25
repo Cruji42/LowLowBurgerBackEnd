de82b9aeef35f878d9bf7eb8483b9c1ba8829d54
Cruji42 <cruji42@gmail.com>
2020-11-24 19:18:17 -0600

    Get orders

--- src/controllers/order_controller.ts
+++ src/controllers/order_controller.ts
@@ -10,7 +10,24 @@ export const getOrders = async (req: Request, res: Response): Promise<Response> 
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     try {
-        const response: QueryResult = await pool.query('SELECT * FROM public.order');
+        let sql='Select distinct t5.folio orden, t6.name, t5.delivery_address, \n' +
+            '(select array[t2.amount::text, t1.name, t2.instructions, array_agg(t4.name)::text])\n' +
+            'from products as t1\n' +
+            'join order_item as t2 on t1.id = t2.products_id\n' +
+            'join order_item_has_toppings t3 on t3.order_item_order_id = t2.id\n' +
+            'join toppings as t4 on t4.id = t3.toppings_id\n' +
+            'join public.order as t5 on t5.id = t2.order_id\n' +
+            'join users as t6 on t6.id = t5.user_id\n' +
+            'group by\n' +
+            't1.name,\n' +
+            't2.amount,\n' +
+            't2.instructions,\n' +
+            't2.id,\n' +
+            't5.folio,\n' +
+            't6.name,\n' +
+            't5.delivery_address\n' +
+            'order by t5.folio';
+        const response: QueryResult = await pool.query(sql);
         console.log(response.rows);
         return res.status(200).json(response.rows);
     } catch (error) {
